8efa3810e5d7accdd4a3e78b839c98b97262a7b1
Andypro1 <871829+Andypro1@users.noreply.github.com>
2024-09-11 17:00:25 -0400

    Add the minimum amount of dependencies to get the z1 standalone build working. Overlays do not work which is fine for my purposes. Update build_z1.bat to output wla debug symbols.

--- build_z1.bat
+++ build_z1.bat
@@ -1,3 +1,3 @@
 @echo off
 del build\main_z1.sfc
-resources\asar.exe src/z1/standalone.asm build/main_z1.sfc
+resources\asar.exe --symbols=wla --symbols-path=build\main_z1.sym src\z1\standalone.asm build\main_z1.sfc
--- src/z1/standalone.asm
+++ src/z1/standalone.asm
@@ -76,11 +76,14 @@ IsrReset:
 IsrNmi:
     JML [$0811]
 
+;  Non-rom dependencies
+incsrc "../macros.asm"
+incsrc "../defines.asm"
+
 incsrc "labels.asm"
 
 ; Include hooks
 incsrc "hooks.asm"
-
 
 ; Include common code (will be copied to WRAM $1000-$1FFF when switching to M1)
 ; The reason for this is that the main "common" MMC1 bank at $C000-$FFFF is more or less full
@@ -94,9 +97,32 @@ warnpc $A7C000
 org $A88000
 incsrc "init.asm"
 incsrc "snes.asm"
+; Include randomizer additions
+; incsrc "randomizer/main.asm"
+
+; Common NES code/data
+namespace nes
+org $9F8000
+incsrc "../common/nes/overlay.asm"
+warnpc $9FFFFF
+namespace off
 
 ; Include randomizer additions
 org $A99000
 incsrc "../nes-spc/spc.asm"
 org $AFFFFF
 db $00
+
+; namespace mb
+; ; free space for more code here
+; warnpc $FFE000
+
+
+; org $FFE000
+; base $40E000
+; incsrc "../motherbrain/snes.asm"
+; incsrc "../motherbrain/sa1.asm"
+; incsrc "../motherbrain/transition.asm"
+; incsrc "../motherbrain/randomizer/main.asm"
+; print "MotherBrain ends = ", pc
+; namespace off
