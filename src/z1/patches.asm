f7f501226813874af190cd7930cb2371e1a18444
Andypro1 <871829+Andypro1@users.noreply.github.com>
2025-07-08 21:35:31 -0400

    Add a z1 patch for faster heart refill qol.

--- src/z1/patches.asm
+++ src/z1/patches.asm
@@ -2,6 +2,7 @@
 
 !B0 = ((!BASE_BANK)<<16)
 !B1 = ((!BASE_BANK+$1)<<16)
+!B5 = ((!BASE_BANK+$5)<<16)
 !B7 = ((!BASE_BANK+$7)<<16)
 
 ;  Fast npc text
@@ -9,3 +10,22 @@
 org !B1+$881d
 ;arch 6502
 db $02
+
+
+;  Z3-speed faster heart refills
+; $B1E6 [World_FillHearts]:  [original code from z1 disassembly]
+; 171E6  A5 63          LDA $63
+; 171E8  F0 29          BEQ $B213
+; 171EA  A9 10          LDA #$10
+; 171EC  8D 04 06       STA $0604
+; 171EF  AD 70 06       LDA $0670
+; 171F2  C9 F8          CMP #$F8    ;  Changing #$F8 to #$eb
+; 171F4  B0 07          BCS $B1FD
+; 171F6  18             CLC
+; 171F7  69 06          ADC #$06    ;  Changing #$06 to #$14
+; 171F9  8D 70 06       STA $0670
+; 171FC  60             RTS
+org !B5+$b1f3
+db $eb
+org !B5+$b1f8
+db $14
