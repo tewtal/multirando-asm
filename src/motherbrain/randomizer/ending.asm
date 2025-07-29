9ea648486ae59e177e4d4bbc1960b767a6c1208f
Andypro1 <871829+Andypro1@users.noreply.github.com>
2025-07-29 15:45:59 -0400

    Add a set_enabled_games_for_legacy_builds subroutine to motherbrain for compatibility purposes while we're maintaining multiple seed-generating versions (0.2.x and 0.1.x).
    This facilitates patch testing and allows the baseline rom build to continue working in legacy builds.

--- src/motherbrain/randomizer/ending.asm
+++ src/motherbrain/randomizer/ending.asm
@@ -1,8 +1,11 @@
 ; Sets the ending flag (in A) for the current game and check if all other requirements are met
 ; If true, returns 1 in A if all games are completed, otherwise 0
-check_and_set_ending:   ; rom ~007ff57e
+check_and_set_ending:
     php
     rep #$20
+
+    jsr set_enabled_games_for_legacy_builds
+
     and.w #$00ff
     cmp.w #$0001
     bne +
@@ -24,7 +27,7 @@ check_and_set_ending:   ; rom ~007ff57e
 +   lda.l config_z3
     beq +
         lda.l !SRAM_ALTTP_COMPLETED : and.w !IRAM_ENDING_TEMP : sta.w !IRAM_ENDING_TEMP
-+   lda.l config_z1        
++   lda.l config_z1
     beq +
         lda.l !SRAM_Z1_COMPLETED : and.w !IRAM_ENDING_TEMP : sta.w !IRAM_ENDING_TEMP
 +   lda.l config_m1
@@ -35,9 +38,37 @@ check_and_set_ending:   ; rom ~007ff57e
     lda.w !IRAM_ENDING_TEMP
     rtl
 
+
+;  Sets the "enabled games" words for quad versions prior to when games were toggleable
+;    This routine can be removed when seeds can no longer be generated for those versions
+set_enabled_games_for_legacy_builds:
+    pha : phx
+    ldx #$0002
+-
+    lda.l config_start, x
+    bne .done   ;  Nonzero data found; we're in a modern build
+    inx : inx
+    cpx.l #$000a
+    bcc -
+
+    ;  All zeros found; we're in a legacy build
+    ldx #$0002
+-
+    lda.l #$0001
+    sta.l config_start, x
+    inx : inx
+    cpx.l #$000a
+    bcc -
+.done
+    plx : pla
+rts
+
 check_other_games_ending:
     phx : php
     rep #$20
+
+    jsr set_enabled_games_for_legacy_builds
+
     and #$00ff : tax
     lda.w #$0001 : sta.w !IRAM_ENDING_TEMP
 
@@ -68,4 +99,4 @@ check_other_games_ending:
 
     plp : plx
     lda.w !IRAM_ENDING_TEMP
-    rtl     ; rom ~007ff666
+    rtl
