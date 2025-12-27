a3ce8a50e2ea0da8acd13f74ea305f592a9fde6c
871829+Andypro1@users.noreply.github.com <871829+Andypro1@users.noreply.github.com>
2025-12-26 20:01:03 -0500

    Redo square channel memory layout; combine the 6 boolean state values into a state flags byte for space. Implement constant volume. Implement Sweep Init and Tick. Implement Sweep updateTargetPeriod calculation routine.

--- src/nes-spc/channels/pulse.asm
+++ src/nes-spc/channels/pulse.asm
@@ -32,39 +32,66 @@ pulse3b: incsrc "../samples/pl3-3.asm"
 ;  $90->$9f: Square 0 internal state
 ;  $a0->$af: Square 1 internal state
 
-sq0Envelope = $90
-sq0TimerTicks = $91
-sq0Duty = $92
-sq0SweepEnabled = $93
-sq0SweepPeriod = $94
-sq0SweepNegate = $95
-sq0SweepShift = $96
-sq0ReloadSweep = $97
-sq0SweepDivider = $98
-sq0SweepDividerPeriod = $99
-sq0RealPeriod = $9a
-sq0Volume     = $9b
-sq0Halted     = $9c     ;  #$20 if set; #$00 if clear
-sq0Start      = $9d
-sq0EnvelopeDivider = $9e
-sq0EnvelopeCounter = $9f
+sq0Duty = $90
+sq0SweepPeriod = $91
+sq0SweepShift = $92
+sq0SweepDivider = $93
+sq0RealPeriodLo = $94
+sq0RealPeriodHi = $95
+sq0Volume     = $96
+sq0EnvelopeDivider = $97
+sq0EnvelopeCounter = $98
+sq0LengthCounter   = $99
+sq0LengthReloadValue = $9a
+sq0LengthPreviousValue = $9b
+sq0TargetPeriodLo = $9c
+sq0TargetPeriodHi = $9d
 
-sq1Envelope = $a0
-sq1TimerTicks = $a1
-sq1Duty = $a2
-sq1SweepEnabled = $a3
-sq1SweepPeriod = $a4
-sq1SweepNegate = $a5
-sq1SweepShift = $a6
-sq1ReloadSweep = $a7
-sq1SweepDivider = $a8
-sq1SweepDividerPeriod = $a9
-sq1RealPeriod = $aa
-sq1Volume     = $ab
-sq1Halted     = $ac     ;  #$20 if set; #$00 if clear
-sq1Start      = $ad
-sq1EnvelopeDivider = $ae
-sq1EnvelopeCounter = $af
+sq0StateFlags = $9f  ;  Channel state boolean flags:
+;  d--- ---- :
+!SweepEnabled = %10000000
+!sq0SweepEnabledFlag   = "sq0StateFlags.7"
+!sq1SweepEnabledFlag   = "sq1StateFlags.7"
+;  -d-- ---- :  Unused
+;  --d- ---- :
+!LengthHalt   = %00100000
+!sq0LengthHaltFlag   = "sq0StateFlags.5"
+!sq1LengthHaltFlag   = "sq1StateFlags.5"
+;  ---d ---- :  
+!ConstantVolume = %00010000
+!sq0ConstantVolumeFlag   = "sq0StateFlags.4"
+!sq1ConstantVolumeFlag   = "sq1StateFlags.4"
+;  ---- d--- :  
+!SweepNegate = %00001000
+!sq0SweepNegateFlag   = "sq0StateFlags.3"
+!sq1SweepNegateFlag   = "sq1StateFlags.3"
+;  ---- -d-- :  Unused
+;  ---- --d- :  
+!ReloadSweep = %00000010
+!sq0ReloadSweepFlag   = "sq0StateFlags.1"
+!sq1ReloadSweepFlag   = "sq1StateFlags.1"
+;  ---- ---d : 
+!EnvelopeStart  = %00000001
+!sq0EnvelopeStartFlag   = "sq0StateFlags.0"
+!sq1EnvelopeStartFlag   = "sq1StateFlags.0"
+
+
+sq1Duty = $a0
+sq1SweepPeriod = $a1
+sq1SweepShift = $a2
+sq1SweepDivider = $a3
+sq1RealPeriodLo = $a4
+sq1RealPeriodHi = $a5
+sq1Volume     = $a6
+sq1EnvelopeDivider = $a7
+sq1EnvelopeCounter = $a8
+sq1LengthCounter   = $a9
+sq1LengthReloadValue = $aa
+sq1LengthPreviousValue = $ab
+sq1TargetPeriodLo = $ac
+sq1TargetPeriodHi = $ad
+
+sq1StateFlags = $af  ;  Channel state boolean flags (see sq0 above)
 
 ;  Methods
 Pulse:
@@ -88,13 +115,39 @@ Pulse:
     rep 6 : lsr a
     mov sq0Duty+x, a
 
-    ;  Set halted
+    ;  Set halt
+...setHalt:
     mov a, y
-    and a, #$20
-    mov sq0Halted+x, a
+    and  a, #$20
+    cmp x, !Square0Offset
+    bne ....sq1
+    clr1 !sq0LengthHaltFlag
+    or a, sq0StateFlags
+    mov  sq0StateFlags, a
+    bra +
+....sq1:
+    clr1 !sq1LengthHaltFlag
+    or a, sq1StateFlags
+    mov  sq1StateFlags, a
++
+
+    ;  Set constant volume
+...setConstantVolume:
+    mov a, y
+    and a, #$10
+    cmp x, !Square0Offset
+    bne ....sq1
+    clr1 !sq0ConstantVolumeFlag
+    or a, sq0StateFlags
+    mov  sq0StateFlags, a
+    bra +
+....sq1:
+    clr1 !sq1ConstantVolumeFlag
+    or a, sq1StateFlags
+    mov  sq1StateFlags, a
++
 
     ;  Set volume
-    ;  TODO: constantvolume(?)
     mov a, y
     and a, #$0f
     mov sq0Volume+x, a
@@ -103,27 +156,34 @@ Pulse:
 
 ;  Tick the envelope
 ..Tick:
-    mov a, sq0Start+x
-    beq ...stopped                  ;  if not start, then
+    mov a, sq0StateFlags+x
+    and a, #!EnvelopeStart
+    bne ...stopped                  ;  if not start, then
     dec sq0EnvelopeDivider+x        ;  divider--
     bpl ...end
     mov a, sq0Volume+x
     mov sq0EnvelopeDivider+x, a     ;  divider = volume
 
-    mov a, sq0EnvelopeCounter
+    mov a, sq0EnvelopeCounter+x
     beq ...counterzero
     dec sq0EnvelopeCounter+x        ;  counter--
     bra ...end
 ...counterzero:
-    mov a, sq0Halted+x
+    mov a, sq0StateFlags+x
+    and a, #!LengthHalt
     beq ...end                      ;  if halted, then
     mov a, #$0f
     mov sq0EnvelopeCounter+x, a     ;  counter = 15
     bra ...end
 
 ...stopped:
-    mov a, #$00
-    mov sq0Start+x, a               ;  start = false
+    cmp x, !Square0Offset
+    bne ...sq1
+    clr1 !sq0EnvelopeStartFlag      ;  start = false
+    bra +
+...sq1:
+    clr1 !sq1EnvelopeStartFlag      ;  start = false
++
     mov a, #$0f
     mov sq0EnvelopeCounter+x, a     ;  counter = 15
     mov a, sq0Volume+x
@@ -143,13 +203,31 @@ ret
     mov y, a
 
     ;  Set sweep enabled
+...sweepEnable:
     and a, #$80
-    mov sq0SweepEnabled+x, a
+    beq ....disable
+    mov a, sq0StateFlags+x
+    or a, #!SweepEnabled
+    bra +
+....disable:
+    mov a, sq0StateFlags+x
+    and a, #~!SweepEnabled
++
+    mov sq0StateFlags+x, a
 
     ;  Set sweep negate
+...sweepNegate:
     mov a, y
     and a, #$08
-    mov sq0SweepNegate+x, a
+    beq ....disable
+    mov a, sq0StateFlags+x
+    or a, #!SweepNegate
+    bra +
+....disable:
+    mov a, sq0StateFlags+x
+    and a, #~!SweepNegate
++
+    mov sq0StateFlags+x, a
 
     ;  Set sweep period
     mov a, y
@@ -163,25 +241,115 @@ ret
     and a, #$07
     mov sq0SweepShift+x, a
 
+    call .._UpdateTargetPeriod
 
-    ;  Update target period
-    ; ShiftResult
-    ; TODO: calc lookup tables
+    ;  Set reloadSweep = true
+    mov a, sq0StateFlags+x
+    or a, #!ReloadSweep
+    mov sq0StateFlags+x, a
+
+    jmp ProcessWrites_handlerReturn
+
+
+;  Tick the sweep
+..Tick:
+    dec sq0SweepDivider+x
+    bne ...reloadSweep
+    mov a, sq0SweepShift+x
+    beq ...done
+    mov a, sq0StateFlags+x
+    and a, #!SweepEnabled
+    beq ...done
+    mov a, sq0TargetPeriodHi+x
+    and a, #$f8
+    bne ...done
+    mov a, sq0RealPeriodLo+x
+    and a, #$f8
+    bne ...setPeriod
+    mov a, sq0RealPeriodHi+x
+    beq ...done
+
+...setPeriod:
+    mov a, sq0TargetPeriodLo+x
+    mov sq0RealPeriodLo+x, a
+    mov a, sq0TargetPeriodHi+x
+    mov sq0RealPeriodHi+x, a    ; realperiod = targetperiod
+
+    call .._UpdateTargetPeriod
+
+...done:
+    mov a, sq0SweepPeriod+x
+    mov sq0SweepDivider+x, a    ; divider = period
+
+...reloadSweep:
+    mov a, sq0StateFlags+x
+    and a, #!ReloadSweep
+    beq +
+    mov a, sq0SweepPeriod+x
+    mov sq0SweepDivider+x, a    ; divider = period
++
+ret
+
+
+.._UpdateTargetPeriod:
+    ; TODO (no): calc lookup tables
     ;  old spc uses SNESTABL, a *4096* byte lookup table
     ;  we need to either burn cycles and do the 16-bit math,
     ;  or generate an 7+3-bit lookup table: 1024 bytes I guess?
 
-    mov a, #$01
-    mov sq0ReloadSweep+x, a
+    ;  Load period heap memory
+    ShiftResultLo = $00
+    ShiftResultHi = $01
 
-    nop
-    nop
-    jmp ProcessWrites_handlerReturn
+    mov a, sq0RealPeriodLo+x
+    mov ShiftResultLo, a
+    mov a, sq0RealPeriodHi+x
+    mov ShiftResultHi, a
 
-;  Tick the sweep for the pulse channel flag in [A]
-..Tick:
+...sweepShift:
+    mov sq0SweepShift+x, a
+    beq ....done          ; 0-bit shift → no work
 
+....loop:
+    lsr ShiftResultHi           ; shift high byte right
+    ror ShiftResultLo             ; rotate carry into low byte
+    dec a
+    bne ....loop
+....done:
+
+    mov a, sq0StateFlags+x
+    and a, #!SweepNegate
+    beq ....add
+....subtract:
+    setc
+
+    mov a, sq0RealPeriodLo+x
+    sbc a, ShiftResultLo
+    mov sq0TargetPeriodLo+x, a
+
+    mov a, sq0RealPeriodHi+x
+    sbc a, ShiftResultHi
+    mov sq0TargetPeriodHi+x, a
+
+    cmp x, #$00
+    beq +
+    dec sq0TargetPeriodLo+x     ;  sweep target period -1
+    ;  TODO: check above dec for underflow
+    bra +
+
+....add:
+    clrc
+
+    mov a, sq0RealPeriodLo+x
+    adc a, ShiftResultLo
+    mov sq0TargetPeriodLo+x, a
+
+    mov a, sq0RealPeriodHi+x
+    adc a, ShiftResultHi
+    mov sq0TargetPeriodHi+x, a
++
 ret
+
 
 .Volume:
 
