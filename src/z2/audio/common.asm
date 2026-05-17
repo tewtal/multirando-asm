02409b10cc67bec7b64fcea9ac2e01889417cc54
Andypro1 <871829+Andypro1@users.noreply.github.com>
2026-05-16 22:22:43 -0400

    Move SnesUpdateAudio to audio/common.asm. Implement new nes apu. Add all the $40xx hooks found in the z2 source (wow, there are excessively many!).

--- src/z2/audio/common.asm
+++ src/z2/audio/common.asm
@@ -1,88 +1,223 @@
 print "z2-apu-routines = ", pc
 
+!ApuWritesIndex  = $090f
+!ApuNumberWrites = $0910
+!ApuValueWrites  = $0940
+!ApuIo0          = $2140
+!ApuIo1          = $2141
+!ApuIo2          = $2142
+!ApuIo3          = $2143
+!ApuPacketSeq    = $0970
+!SpcReadyValue   = #$7d
+!CpuReadyValue   = #$d7
+
+; APU Update routines
+SnesUpdateAudio:
+    phx : phy : pha : php
+    sep #$30
+
+    ldy !ApuWritesIndex   ;  Set up decrementing loop index in [Y]
+    beq .end              ;  no queued writes, skip handshake/transfer
+
+    ;  cpu<->apu handshake
+    lda !ApuIo0
+    cmp !SpcReadyValue    ;  Check for spc readiness
+    beq +
+    jmp .end
++
+
+    sty !ApuIo2           ;  Send transfer length
+    lda !CpuReadyValue    ;  Indicate cpu readiness
+    sta !ApuIo0
+
+-   lda !ApuIo0           ;  Wait for SPC to ack CPU ready
+    cmp !CpuReadyValue
+    bne -
+
+    ldx #$00
+    stz !ApuPacketSeq
+.transferLoop:
+    lda !ApuValueWrites,x
+    sta !ApuIo1           ;  Send first apu value
+
+    cpy #$01
+    beq .sendSingle
+
+    inx
+    lda !ApuNumberWrites,x
+    sta !ApuIo2           ;  Send second apu index
+
+    lda !ApuValueWrites,x
+    sta !ApuIo3           ;  Send second apu value
+
+    dex
+    lda !ApuPacketSeq
+    ora !ApuNumberWrites,x
+    sta !ApuIo0           ;  Send packet sequence + first apu index
+
+    inx                   ;  Next write index
+    inx
+    dey
+    dey
+    beq .finishedTransfer
+
+    lda !ApuPacketSeq
+    clc
+    adc #$20
+    and #$e0
+    sta !ApuPacketSeq
+
+    ;  spc packet ack wait loop
+-   lda !ApuIo0
+    and #$e0
+    cmp !ApuPacketSeq
+    bne -
+
+    bra .transferLoop
+
+.sendSingle:
+    lda !ApuPacketSeq
+    ora !ApuNumberWrites,x
+    sta !ApuIo0           ;  Send packet sequence + first apu index
+
+.finishedTransfer:
+    stz !ApuWritesIndex     ;  Reset queue
+
+.end
+    plp : pla : ply : plx
+    rtl
+
+;  Params:
+;   [A] has the apu register number
+;   [B] has the apu register value
+;  Caller should preserve original [A] outside the routine if needed
+EnqueueApuWrite:
+    phx     ;  Preserve [X]
+    ldx !ApuWritesIndex
+    sta !ApuNumberWrites,x     ;  Write reg. number
+    xba
+    sta !ApuValueWrites,x     ;  Write reg. value
+    inx
+    stx !ApuWritesIndex       ;  Update index
+    plx
+    rts
+
 ;  Group of methods for the square wave 0 channel
 Sq0:
 
 ;  Methods and properties for duty
 .Duty
 
+!apuReg = #$00 ;$4000
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
 ..WriteX:
-    stx.w z2_Sq0Duty_4000
+    php
+    pha
+    txa
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
     rts
 
 ..WriteY:
-    sty.w z2_Sq0Duty_4000
+    php
+    pha
+    tya
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
     rts
 
 ;  Methods and properties for pitch sweep
 .Sweep
 
+!apuReg = #$01 ;$4001
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
 ..WriteY:
+    php
+    pha
+    tya
     xba
-    lda #$40
-    tsb.w z2_APUExtraControl
-    xba
-    sty.w z2_Sq0Sweep_4001
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
     rts
 
 ;  Methods and properties for the timer
 .Timer
 
+!apuReg = #$02 ;$4002
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
+..WriteX:
+    php
+    pha
+    txa
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
+    rts
+
 ..WriteXIndexed:
-    cpx #$00
-    bne ...next
-    sta z2_Sq0Timer_4002
-...next:
-    cpx #$04
-    bne ...next2
-    sta z2_Sq1Timer_4006
-...next2:
-    cpx #$08
-    bne ...done
-    sta z2_TrgTimer_400A
-...done:
+    php
+    xba
+    txa     ;  [X] has the apu base reg.
+    inc : inc ; base+!apuReg
+
+    jsr EnqueueApuWrite
+    plp
     rts
 
 ;  Methods and properties for note length
 .Length
 
+!apuReg = #$03 ;$4003
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
 ..WriteXIndexed:
-    pha : phx
-    cpx #$00
-    bne ...next
-    sta z2_Sq0Length_4003
-    tax
-    lda.w Sound__EmulateLengthCounter_length_d3_mixed, x
-    sta.w z2_APUSq0Length ; $920 - not a well-named variable..
+    php
     xba
-    lda #$01
-    tsb.w z2_ApuStatus_4015
-    tsb.w z2_APUExtraControl
-    bra ...done
-...next:
-    cpx #$04
-    bne ...next2
-    sta z2_Sq1Length_4007
-    tax
-    lda.w Sound__EmulateLengthCounter_length_d3_mixed, x
-    sta.w z2_APUSq1Length ; $922 - not a well-named variable..
-    xba
-    lda #$02
-    tsb.w z2_ApuStatus_4015
-    tsb.w z2_APUExtraControl
-    bra ...done
-...next2:
-    cpx #$08
-    bne ...done
-    sta z2_TrgLength_400B
-    tax
-    lda #$04
-    tsb.w z2_APUExtraControl
-    tsb.w z2_ApuStatus_4015
-    lda.w Sound__EmulateLengthCounter_length_d3_mixed, x
-    sta.w z2_APUTriLength ; $924 - not a well-named variable..
-...done:
-    plx : pla
+    txa     ;  [X] has the apu reg.
+    inc : inc : inc ; base+!apuReg
+    jsr EnqueueApuWrite
+    plp
     rts
 
 ;  Group of methods for the square wave 1 channel
@@ -91,19 +226,73 @@ Sq1:
 ;  Methods and properties for duty
 .Duty
 
+!apuReg = #$04 ;$4004
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
 ..WriteX:
-    stx.w z2_Sq1Duty_4004
+    php
+    pha
+    txa
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
     rts
 
 ;  Methods and properties for pitch sweep
 .Sweep
 
+!apuReg = #$05 ;$4005
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
 ..WriteY:
+    php
+    pha
+    tya
     xba
-    lda #$80
-    tsb.w z2_APUExtraControl
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
+    rts   
+
+.Timer
+
+!apuReg = #$06 ;$4006
+
+..WriteA:
+    php
     xba
-    sty.w z2_Sq1Sweep_4005
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
+.Length
+
+!apuReg = #$07 ;$4007
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
     rts
 
 ;  Group of methods for the triangle channel
@@ -112,9 +301,38 @@ Tri:
 ;  Methods and properties for linear
 .Linear
 
-..WriteY:
-    sty.w z2_TrgLinear_4008
+!apuReg = #$08 ;$4008
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
     rts
+
+
+..WriteX:
+    php
+    pha
+    txa
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
+    rts
+
+..WriteY:
+    php
+    pha
+    tya
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
+    rts   
 
 ;  Group of methods for the noise channel
 Noise:
@@ -122,142 +340,146 @@ Noise:
 ;  Methods and properties for volume
 .Volume
 
+!apuReg = #$0c ;$400c
+
 ..WriteA:
-    sta z2_NoiseVolume_400C
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
+..WriteX:
+    php
+    pha
+    txa
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
     rts
 
 ;  Methods and properties for noise frequency
 .Period
 
+!apuReg = #$0e ;$400e
+
 ..WriteA:
-    sta z2_NoisePeriod_400E
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
     rts
 
 ..WriteX:
-    stx.w z2_NoisePeriod_400E
+    php
+    pha
+    txa
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
     rts
 
 ;  Methods and properties for note length
 .Length
 
+!apuReg = #$0f ;$400f
+
 ..WriteA:
-    phx
-    sta z2_NoiseLength_400F
-    tax
-    lda #$08
-    tsb.w z2_APUExtraControl
-    tsb.w z2_ApuStatus_4015
-    lda.w Sound__EmulateLengthCounter_length_d3_mixed, x
-    sta.w z2_APUNoiLength ; $0926 - not a well-named variable..
-    txa
-    plx
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
     rts
 
 ..WriteY:
-    phx
-    sty.w z2_NoiseLength_400F
-    tax
-    lda #$08
-    tsb.w z2_APUExtraControl
-    tsb.w z2_ApuStatus_4015
-    lda.w Sound__EmulateLengthCounter_length_d3_mixed, x
-    sta.w z2_APUNoiLength ; $0926 - not a well-named variable..
+    php
+    pha
+    tya
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
+    rts   
+
+Dmc:
+
+.Frequency
+
+!apuReg = #$10 ;$4010
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
+.Address
+
+!apuReg = #$12 ;$4012
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
+.Length
+
+!apuReg = #$13 ;$4013
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
+Apu:
+
+.Control
+
+!apuReg = #$15 ;$4015
+
+..WriteA:
+    php
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
+    rts
+
+..WriteX:
+    php
+    pha
     txa
-    plx
+    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    pla
+    plp
     rts
 
+.FrameCounter
 
-z2_WriteAPUControl:
-    sta.w z2_APUIOTemp
+!apuReg = #$17 ;$4017
+
+..WriteA:
+    php
     xba
-    lda.w z2_APUIOTemp
-    eor.b #$ff
-    and.b #$1f
-    trb.w z2_APUBase+$15
-    trb.w z2_APUExtraControl
-    lsr.w z2_APUIOTemp
-    bcs +
-        stz.w z2_APUBase+$03
-        stz.w z2_APUSq0Length
-+
-    lsr.w z2_APUIOTemp
-    bcs +
-        stz.w z2_APUBase+$07
-        stz.w z2_APUSq1Length
-+
-    lsr.w z2_APUIOTemp
-    bcs +
-        stz.w z2_APUBase+$0B
-        stz.w z2_APUTriLength
-+
-    lsr.w z2_APUIOTemp
-    bcs +
-        stz.w z2_APUBase+$0F
-        stz.w z2_APUNoiLength
-+
-    lsr.w z2_APUIOTemp
-    bcc +
-        lda.b #$10
-        tsb.w z2_APUBase+$15
-        bne +
-            tsb.w z2_APUExtraControl
-+
-    xba
+    lda !apuReg
+    jsr EnqueueApuWrite
+    plp
     rts
-
-; WriteAPUDMCCounter:
-;     stx.w DmcCounter_4011
-; rts
-
-; WriteAPUDMCFreq:
-;     sta DmcFreq_4010
-; rts
-
-; WriteAPUDMCAddr:
-;     sta DmcAddress_4012
-; rts
-
-; WriteAPUDMCLength:
-;     sta DmcLength_4013
-; rts
-
-; WriteAPUDMCPlay:
-;     sta ApuStatus_4015
-;     and #%00010000
-;     sta APUExtraControl
-; rts
-
-
-Sound__EmulateLengthCounter_length_d3_mixed:
-fillbyte $06 : fill 8
-fillbyte $80 : fill 8
-fillbyte $0B : fill 8
-fillbyte $02 : fill 8
-fillbyte $15 : fill 8
-fillbyte $03 : fill 8
-fillbyte $29 : fill 8
-fillbyte $04 : fill 8
-fillbyte $51 : fill 8
-fillbyte $05 : fill 8
-fillbyte $1F : fill 8
-fillbyte $06 : fill 8
-fillbyte $08 : fill 8
-fillbyte $07 : fill 8
-fillbyte $0F : fill 8
-fillbyte $08 : fill 8
-fillbyte $07 : fill 8
-fillbyte $09 : fill 8
-fillbyte $0D : fill 8
-fillbyte $0A : fill 8
-fillbyte $19 : fill 8
-fillbyte $0B : fill 8
-fillbyte $31 : fill 8
-fillbyte $0C : fill 8
-fillbyte $61 : fill 8
-fillbyte $0D : fill 8
-fillbyte $25 : fill 8
-fillbyte $0E : fill 8
-fillbyte $09 : fill 8
-fillbyte $0F : fill 8
-fillbyte $11 : fill 8
-fillbyte $10 : fill 8
--- src/z2/audio/hooks.asm
+++ src/z2/audio/hooks.asm
@@ -5,93 +5,150 @@ optimize address ram
 !B7 = ((!BASE_BANK+$7)<<16)
 
 ;  APU control calls
-org !B6+$8007 : jsr z2_WriteAPUControl
-org !B6+$9292 : jsr z2_WriteAPUControl
-org !B6+$9298 : jsr z2_WriteAPUControl
-org !B6+$985a : jsr z2_WriteAPUControl
+org !B6+$8007 : jsr Apu_Control_WriteA
+org !B6+$9261 : jsr Apu_Control_WriteA
+org !B6+$9292 : jsr Apu_Control_WriteA
+org !B6+$9298 : jsr Apu_Control_WriteA
+org !B6+$92b9 : jsr Apu_Control_WriteX
+org !B6+$985a : jsr Apu_Control_WriteA
 
+;  Frame counter calls
+org !B6+$8002 : jsr Apu_FrameCounter_WriteA
+org !B6+$900D : jsr Apu_FrameCounter_WriteA
 
 ;  Square wave 0 calls
+org !B6+$810f : jsr Sq0_Duty_WriteX
+org !B6+$820e : jsr Sq0_Duty_WriteA
+org !B6+$8250 : jsr Sq0_Duty_WriteA
+org !B6+$82a6 : jsr Sq0_Duty_WriteX
 org !B6+$9031 : jsr Sq0_Duty_WriteX
-;  Possible other calls:
-; 1810f
-; 182a6
-; 192b1
+org !B6+$92b1 : jsr Sq0_Duty_WriteX
+org !B6+$935c : jsr Sq0_Duty_WriteA
+org !B6+$9367 : jsr Sq0_Duty_WriteA
+org !B6+$93aa : jsr Sq0_Duty_WriteA
+org !B6+$9479 : jsr Sq0_Duty_WriteA
+org !B6+$9495 : jsr Sq0_Duty_WriteA
+org !B6+$97ac : jsr Sq0_Duty_WriteA
+org !B6+$980e : jsr Sq0_Duty_WriteA
+org !B6+$9870 : jsr Sq0_Duty_WriteA
+org !B6+$98dc : jsr Sq0_Duty_WriteA
+org !B6+$98f5 : jsr Sq0_Duty_WriteA
+org !B6+$99c8 : jsr Sq0_Duty_WriteA
+org !B6+$9acb : jsr Sq0_Duty_WriteA
+org !B6+$9b45 : jsr Sq0_Duty_WriteA
+
+org !B6+$8112 : jsr Sq0_Sweep_WriteY
+org !B6+$8243 : jsr Sq0_Sweep_WriteA
 org !B6+$9034 : jsr Sq0_Sweep_WriteY
-;  Possible other calls:
-; 18112
+org !B6+$9349 : jsr Sq0_Sweep_WriteA
+org !B6+$97e9 : jsr Sq0_Sweep_WriteA
+org !B6+$989b : jsr Sq0_Sweep_WriteA
+org !B6+$98d1 : jsr Sq0_Sweep_WriteA
+org !B6+$99f3 : jsr Sq0_Sweep_WriteA
+org !B6+$9a29 : jsr Sq0_Sweep_WriteA
+org !B6+$9a51 : jsr Sq0_Sweep_WriteA
+org !B6+$9a79 : jsr Sq0_Sweep_WriteA
+
+org !B6+$8128 : jsr Sq0_Timer_WriteXIndexed
+org !B6+$8188 : jsr Sq0_Timer_WriteXIndexed
+org !B6+$81e7 : jsr Sq0_Timer_WriteA
+org !B6+$8256 : jsr Sq0_Timer_WriteA
 org !B6+$904a : jsr Sq0_Timer_WriteXIndexed
-;  Possible other calls:
-; 18128
-; 18188
-
-org !B6+$9060 : jsr Sq0_Length_WriteXIndexed
-;  Possible other calls:
-; 18140
-
 org !B6+$90b1 : jsr Sq0_Timer_WriteXIndexed
+org !B6+$9398 : jsr Sq0_Timer_WriteX
+org !B6+$945f : jsr Sq0_Timer_WriteA
+org !B6+$98ac : jsr Sq0_Timer_WriteA
+org !B6+$9a04 : jsr Sq0_Timer_WriteA
+org !B6+$9ac7 : jsr Sq0_Timer_WriteA
+
+org !B6+$8140 : jsr Sq0_Length_WriteXIndexed
+org !B6+$81f8 : jsr Sq0_Length_WriteA
+org !B6+$8261 : jsr Sq0_Length_WriteA
+org !B6+$9060 : jsr Sq0_Length_WriteXIndexed
+org !B6+$939b : jsr Sq0_Length_WriteA
+org !B6+$946a : jsr Sq0_Length_WriteA
+org !B6+$98b2 : jsr Sq0_Length_WriteA
+org !B6+$9a0a : jsr Sq0_Length_WriteA
+org !B6+$9abf : jsr Sq0_Length_WriteA
+
 org !B7+$cf83 : jsr Sq0_Duty_WriteY
 
-
-; instructions to search for:
 ; sta $40[$00 -> $15] : 8d [00] 40  ; vary [] param up to $15
 ; sta $40[$00 -> $15],x : 9d [00] 40
 ; stx $40[$00 -> $15] : 8e [00] 40
 ; sty $40[$00 -> $15] : 8c [00] 40
 
 ;  Square wave 1 calls
+org !B6+$8116 : jsr Sq1_Duty_WriteX
+org !B6+$8211 : jsr Sq1_Duty_WriteA
+org !B6+$8253 : jsr Sq1_Duty_WriteA
+org !B6+$82a9 : jsr Sq1_Duty_WriteX
 org !B6+$9038 : jsr Sq1_Duty_WriteX
-;  Possible other calls:
-; 18116
-; 182a9
+org !B6+$947c : jsr Sq1_Duty_WriteA
+org !B6+$94da : jsr Sq1_Duty_WriteA
+org !B6+$94e9 : jsr Sq1_Duty_WriteA
+org !B6+$9548 : jsr Sq1_Duty_WriteA
+org !B6+$9b48 : jsr Sq1_Duty_WriteA
+
+org !B6+$8119 : jsr Sq1_Sweep_WriteY
+org !B6+$8246 : jsr Sq1_Sweep_WriteA
 org !B6+$903b : jsr Sq1_Sweep_WriteY
-;  Possible other calls:
-; 18119
 
+org !B6+$81f0 : jsr Sq1_Timer_WriteA
+org !B6+$825c : jsr Sq1_Timer_WriteA
+org !B6+$9462 : jsr Sq1_Timer_WriteA
+org !B6+$94c6 : jsr Sq1_Timer_WriteA
 
+org !B6+$81fb : jsr Sq1_Length_WriteA
+org !B6+$8264 : jsr Sq1_Length_WriteA
+org !B6+$946D : jsr Sq1_Length_WriteA
+org !B6+$94CE : jsr Sq1_Length_WriteA
 
 ;  Sq1 but handled by sq0 routines:
 ; 190B1  9D 02 40       STA Sq0Timer_4002,X
 ; 19060  9D 03 40       STA Sq0Length_4003,X
 
-
 ;  Triangle calls
+org !B6+$829E : jsr Tri_Linear_WriteX
+org !B6+$8481 : jsr Tri_Linear_WriteA
+org !B6+$8488 : jsr Tri_Linear_WriteA
+org !B6+$8498 : jsr Tri_Linear_WriteA
+org !B6+$93E1 : jsr Tri_Linear_WriteA
+org !B6+$9576 : jsr Tri_Linear_WriteX
+org !B6+$9B40 : jsr Tri_Linear_WriteA
 org !B6+$9d63 : jsr Tri_Linear_WriteY
 
 ;  Triangle but handled by sq0 routines:
 ; 1904A  9D 02 40       STA Sq0Timer_4002,X (x==08)
 ; 19060  9D 03 40       STA Sq0Length_4003,X (x==08)
 
-
 ;  Noise calls
-org !B6+$959d : jsr Noise_Length_WriteY
-org !B6+$95a0 : jsr Noise_Period_WriteX
-org !B6+$95a3 : jsr Noise_Volume_WriteA
-;  Possible other calls:
-; 184c5
-; 193dc
-; 19593
-; 197d2
-; 1982e
-; 198fc
-; 19b0a
+org !B6+$82A3 : jsr Noise_Volume_WriteX
+org !B6+$84C5 : jsr Noise_Volume_WriteA
+org !B6+$92B4 : jsr Noise_Volume_WriteX
+org !B6+$93DC : jsr Noise_Volume_WriteA
+org !B6+$9593 : jsr Noise_Volume_WriteA
+org !B6+$95A3 : jsr Noise_Volume_WriteA
+org !B6+$962F : jsr Noise_Volume_WriteA
+org !B6+$97D2 : jsr Noise_Volume_WriteA
+org !B6+$982E : jsr Noise_Volume_WriteA
+org !B6+$98FC : jsr Noise_Volume_WriteA
+org !B6+$9B0A : jsr Noise_Volume_WriteA
+org !B6+$9B4B : jsr Noise_Volume_WriteA
+
+org !B6+$84CB : jsr Noise_Period_WriteA
+org !B6+$95A0 : jsr Noise_Period_WriteX
 org !B6+$9625 : jsr Noise_Period_WriteA
-;  Possible other calls:
-; 184cb
-; 19820
-; 19b00
-org !B6+$962f : jsr Noise_Volume_WriteA
+org !B6+$9820 : jsr Noise_Period_WriteA
+org !B6+$9B00 : jsr Noise_Period_WriteA
+
+org !B6+$84D1 : jsr Noise_Length_WriteA
+org !B6+$959d : jsr Noise_Length_WriteY
 org !B6+$9634 : jsr Noise_Length_WriteA
-;  Possible other calls:
-; 184d1
-; 19826
-; 19b0f
-org !B6+$9b4b : jsr Noise_Volume_WriteA
+org !B6+$9825 : jsr Noise_Length_WriteA
+org !B6+$9B0F : jsr Noise_Length_WriteA
 
-
-;  DMC calls (TODO:)
-; org !B0+$9bb7 : jsr WriteAPUDMCCounter
-; org !B0+$9bcf : jsr WriteAPUDMCFreq
-; org !B0+$9bd5 : jsr WriteAPUDMCAddr
-; org !B0+$9bdb : jsr WriteAPUDMCLength
-; org !B0+$9bea : jsr WriteAPUDMCPlay
+;  DMC calls
+org !B6+$925C : jsr Dmc_Frequency_WriteA
+org !B6+$9251 : jsr Dmc_Address_WriteA
+org !B6+$9257 : jsr Dmc_Length_WriteA
